149c2e2a945026c1a3756ca6210415d1c12cbff5
Ray <startup@9043672@gmail.com>
2024-09-09 15:56:31 +0800

    test:import tailwind css

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,7 +1,6 @@
 <script setup>
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@vue-hero-icons/outline'
-
+import { HomeIcon } from "@vue-hero-icons/solid"
 const navigation = [
   { name: 'Dashboard', href: '#', current: true },
   { name: 'Team', href: '#', current: false },
@@ -19,8 +18,7 @@ const navigation = [
           <DisclosureButton class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
             <span class="absolute -inset-0.5" />
             <span class="sr-only">Open main menu</span>
-            <Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
-            <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
+            <!-- <HomeIcon v-if="!open" class="block h-6 w-6" aria-hidden="true" /> -->
           </DisclosureButton>
         </div>
         <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
@@ -37,7 +35,6 @@ const navigation = [
           <button type="button" class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
             <span class="absolute -inset-1.5" />
             <span class="sr-only">View notifications</span>
-            <BellIcon class="h-6 w-6" aria-hidden="true" />
           </button>
 
           <!-- Profile dropdown -->

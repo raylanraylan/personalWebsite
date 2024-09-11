34bff6b6228021806c712b9bf95a1ea061b774b9
Ray <startup@9043672@gmail.com>
2024-09-11 15:50:47 +0800

    modify:tailwind file setting

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./src/**/**/*.{vue,js,ts,jsx,tsx}",
+
+  ],
+  theme: {
+    screens: {
+      'sm': '568px',
+      // => @media (min-width: 576px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 960px) { ... }
+
+      'lg': '992px',
+      // => @media (min-width: 1440px) { ... }
+
+      'xl': '1200px',
+      // => @media (min-width: 1440px) { ... }
+    },
+    extend: {},
+  },
+  plugins: [],
+}

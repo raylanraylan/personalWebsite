50dfe5a4e8a2288eac7d69dd088d430286be67eb
Ray <startup@9043672@gmail.com>
2024-09-15 14:28:15 +0800

    style:page layout

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import Header from '@/components/layout/Header.vue'
+import Footer from '@/components/layout/Footer.vue';
 </script>
 
 <template>
   <header>
     <Header/>
   </header>
-
-  <RouterView />
+  <main>
+    <RouterView />
+  </main>
+  <footer>
+    <Footer/>
+  </footer>
 </template>
 
 <style scoped>

050e81881aa23320f41a9acd5eb88634ddd07fb1
Ray <startup@9043672@gmail.com>
2024-10-09 22:39:36 +0800

    fix:vite base url

--- vite.config.ts
+++ vite.config.ts
@@ -13,5 +13,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base:'https://raylanraylan.github.io/personalWebsite/'
+  base:'/personalWebsite/'
 })

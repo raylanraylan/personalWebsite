525d23fadedd026bbb4c62de8a1cfe0db6def584
Ray <startup@9043672@gmail.com>
2024-10-13 16:17:49 +0800

    fix:vite config

--- vite.config.ts
+++ vite.config.ts
@@ -20,25 +20,4 @@ export default defineConfig({
     }
   },
   base:'/personalWebsite/',
-  build:{
-    rollupOptions:{
-      output:{
-        chunkFileNames:'assets/js/[name]-[hash],js',
-        entryFileNames:'assets/js/[name]-[hash],js',
-        assetFileNames:'assets/[ext]/[name]-[hash].[ext]',
-        manualChunks(id){
-          if(id.includes('node_modules')){
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        }
-      }
-    },
-    // minify:'terser',
-    // terserOptions:{
-    //   compress:{
-    //     drop_console:true,
-    //     drop_debugger:true
-    //   }
-    // }
-  }
 })

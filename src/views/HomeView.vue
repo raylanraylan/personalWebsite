d19c506ad075dbe47d94a3435874a9f4ded8445a
Ray <startup@9043672@gmail.com>
2024-10-13 13:48:06 +0800

    remove:loading

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,25 +1,25 @@
 <script setup lang="ts">
-import { ref,defineProps,watch } from 'vue';
-const showText = ref<boolean>(false);
-const props = defineProps<{
-  isLoading:boolean
-}>()
+// import { ref,defineProps,watch } from 'vue';
+// const showText = ref<boolean>(false);
+// const props = defineProps<{
+//   isLoading:boolean
+// }>()
 
-watch(() => props.isLoading, (newValue) => {
-  const showContent = setTimeout(()=>{
-    if (!newValue) {
-      showText.value = true;
-      clearTimeout(showContent)
-    }
-  },300)
-}, { immediate: true });
-
+// watch(() => props.isLoading, (newValue) => {
+//   const showContent = setTimeout(()=>{
+//     if (!newValue) {
+//       showText.value = true;
+//       clearTimeout(showContent)
+//     }
+//   },300)
+// }, { immediate: true });
+//  v-if="showText"
 </script>
 
 <template>
   <Transition name="slide-fade">
-    <div v-if="showText">
-      <h2 class="text-[4rem] sm:text-[6rem] leading-tight dark:text-white">{{$t("home.greeting")}}</h2>
+    <div>
+      <h2 class="text-[4rem] sm:text-[8rem] leading-tight dark:text-white">{{$t("home.greeting")}}</h2>
       <p class="dark:text-white">{{$t("home.introduce")}}</p>
     </div>
   </Transition>

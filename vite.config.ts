cd56c2ae4b49fd0ff374e9d9a1e9b0f09ac45d99
Ray <startup@9043672@gmail.com>
2024-10-23 16:54:11 +0800

    chore:vite setting

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,19 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig,loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-const isGithubPages = process.env.VITE_DEPLOY === 'github'
-const isAmplify = process.env.VITE_DEPLOY === 'amplify'
-
 // https://vitejs.dev/config/
-export default defineConfig ({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  base:isGithubPages?`/personalWebsite/`:`/`,
+export default defineConfig (({ command, mode }) =>{
+  const env = loadEnv(mode, process.cwd(), '')
+  return{  
+    plugins: [
+      vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    base:env.VITE_BASE,
+  }
 })
 

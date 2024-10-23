0656ab1ee25c4ef5d24113821de87c9330793531
Ray <startup@9043672@gmail.com>
2024-10-23 17:08:59 +0800

    chore:modify setting

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig (({ command, mode }) =>{
-  const env = loadEnv(mode, process.cwd(), '')
+  const env = loadEnv(mode, process.cwd())
+  console.log(env.VITE_BASE);
   return{  
     plugins: [
       vue(),
@@ -15,7 +16,7 @@ export default defineConfig (({ command, mode }) =>{
         '@': fileURLToPath(new URL('./src', import.meta.url))
       }
     },
-    base:'/personalWebsite/'
+    base:env.VITE_BASE
   }
 })
 

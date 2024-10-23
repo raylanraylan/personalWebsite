1056282c37320e8d4e86a3210817188261d7fc6f
Ray <startup@9043672@gmail.com>
2024-10-23 16:17:43 +0800

    fix:chore use env setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,22 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig,loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  base: process.env.VITE_BASE,
-})
+export default defineConfig((({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [
+      vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    base: env.VITE_BASE,
+  }
+}))
+

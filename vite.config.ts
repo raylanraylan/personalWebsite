5c490bf2bf86c48ad0741ff6cfe93ce2fc94ae18
Ray <startup@9043672@gmail.com>
2024-10-23 22:40:13 +0800

    chore:fix setting

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,33 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig,loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
-process.env.VITE_BASE = process.env.VITE_BASE
 // https://vitejs.dev/config/
+
 export default defineConfig (({ command, mode }) =>{
-  return{  
-    plugins: [
-      vue(),
-    ],
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
-      }
-    },
-    base:process.env.VITE_BASE,
+  if (mode==='github'){
+    return{  
+      plugins: [
+        vue(),
+      ],
+      resolve: {
+        alias: {
+          '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+      },
+      base:'/personalWebsite/',
+    }
+  } 
+
+  if(mode==='aws'){
+    return{  
+      plugins: [
+        vue(),
+      ],
+      resolve: {
+        alias: {
+          '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+      },
+    }
   }
 })

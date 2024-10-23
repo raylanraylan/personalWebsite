8ba045a6cc9144d0b180b64976350768d3872508
Ray <startup@9043672@gmail.com>
2024-10-23 16:05:25 +0800

    feat:create env

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,16 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    visualizer({
-      emitFile: true,
-      filename: "stats.html",
-    }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base:'/personalWebsite/',
+  base: process.env.VITE_BASE,
 })

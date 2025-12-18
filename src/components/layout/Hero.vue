cc22669eece0d83b648f7800e0b42d1a54e23527
Ray <startup@9043672@gmail.com>
2025-12-18 22:15:34 +0800

    feat: add Hero component with call-to-action button

---
+++ src/components/layout/Hero.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Button from '@/components/ui/button/Button.vue'
+</script>
+
+<template>
+  <div>
+    <Button class="cta-button" variant="outline">打開檔案</Button>
+  </div>
+</template>

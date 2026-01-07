86b3a630b85a8ceb146675802e5de9c83443354b
Ray <startup@9043672@gmail.com>
2026-01-07 22:09:52 +0800

    feat: implement interactive lighting effect in Hero component and update HomeView to utilize Hero

--- src/components/layout/Hero.vue
+++ src/components/layout/Hero.vue
@@ -1,9 +1,30 @@
 <script setup lang="ts">
-import Button from '@/components/ui/button/Button.vue'
+import { ref } from 'vue';
+
+const lightingSection = ref<HTMLElement|null>(null);
+const lightPosition = ref<{x:string,y:string}>({x:'0px',y:'0px'});
+
+const mouseMoveHandler = (e:MouseEvent) => {
+  const rect = lightingSection.value?.getBoundingClientRect();
+  if(rect){
+    lightPosition.value.x = ((e.clientX - rect.left) / rect.width) * 100 + '%';
+    lightPosition.value.y = ((e.clientY - rect.top) / rect.height) * 100 + '%';
+  }
+}
+
+document.addEventListener('mousemove', mouseMoveHandler)
 </script>
 
 <template>
-  <div>
-    <Button class="cta-button" variant="outline">打開檔案</Button>
-  </div>
+  <section ref="lightingSection" class="w-full h-screen bg-[url(@/assets/desk.jpeg)] bg-cover bg-center bg-no-repeat">
+    <div class="absolute inset-0 transition-all duration-75" :style="{background: `radial-gradient(400px at ${lightPosition.x} ${lightPosition.y}, transparent 0%, rgba(0, 0, 0, 0.85) 100%)`}"/>
+    <div class="px-[5vw] pt-24 pb-20 max-w-4xl mx-auto flex flex-col">
+      <div class="z-10 text-center">
+        <p class="dark:text-white">{{ $t("home.fileTitle") }}</p>
+        <h1 class="dark:text-white">Ray</h1>
+        <h2 class="text-[4rem] sm:text-[8rem] leading-tight dark:text-white">{{$t("home.greeting")}}</h2>
+        <p class="dark:text-white">{{$t("home.introduce")}}</p>
+      </div>
+    </div> 
+  </section> 
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
+import Hero from '@/components/layout/Hero.vue';
+
 </script>
 
 <template>
-  <div>
-    <h2 class="text-[4rem] sm:text-[8rem] leading-tight dark:text-white">{{$t("home.greeting")}}</h2>
-    <p class="dark:text-white">{{$t("home.introduce")}}</p>
-  </div>
+  <Hero />
 </template>

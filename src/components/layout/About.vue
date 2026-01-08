a9384a9d94fb3ca767720ec3a4e25bb68dd89840
Ray <startup@9043672@gmail.com>
2026-01-08 22:03:48 +0800

    feat: add initial structure for About, Contact, SideProjects, and Skill components

---
+++ src/components/layout/About.vue
@@ -0,0 +1,4 @@
+<script setup lang="ts"></script>
+<template>
+  <!-- About Section Content -->
+</template>
---
+++ src/components/layout/Experience.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+import Card from '../ui/card/Card.vue';
+import CardHeader from '../ui/card/CardHeader.vue';
+import CardTitle from '../ui/card/CardTitle.vue';
+import CardContent from '../ui/card/CardContent.vue';
+</script>
+<template>
+  <Card class="block w-[375px] bg-paper rounded-lg">
+    <CardHeader>
+      <CardTitle>
+        <h3 class="text-lg text-inverse"></h3>
+        <span class="text-sm text-on-paper-label"></span>
+        <br/>
+        <span class="px-2 py-1 text-xs text-primary rounded bg-surface"></span>
+      </CardTitle>
+      <hr class="h-[1px] border-dashed bg-surface"/>
+    </CardHeader>
+    <CardContent>
+      <span></span>
+      <br/>
+      <p></p>
+    </CardContent>
+  </Card>
+</template>

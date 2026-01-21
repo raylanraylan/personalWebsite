4d72618da943f1ecaac21c88d245ef571ab925d0
Ray <startup@9043672@gmail.com>
2026-01-21 20:14:09 +0800

    feat: add padding to footer copyright text for improved spacing

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -3,5 +3,5 @@ const thisYear:number = new Date().getFullYear();
 </script>
 
 <template>
-  <p class="text-center text-sm dark:text-amber-400">{{$t("footer.copyright")}} &copy; {{thisYear}} Ray</p>
+  <p class="text-center py-3 text-sm dark:text-amber-400">{{$t("footer.copyright")}} &copy; {{thisYear}} Ray</p>
 </template>

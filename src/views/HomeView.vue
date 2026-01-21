27bd2626c2a7b2cca1dd0f08fea189b7267158ba
Ray <startup@9043672@gmail.com>
2026-01-21 20:14:37 +0800

    feat: add file number generation and pass it to components for dynamic display

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,31 @@
 <script setup lang="ts">
 import Hero from '@/components/layout/Hero.vue';
+import About from '@/components/layout/About.vue';
+import Skill from '@/components/layout/Skill.vue';
+import Experience from '@/components/layout/Experience.vue';
+import SideProjects from '@/components/layout/SideProjects.vue';
+import Contact from '@/components/layout/Contact.vue';
+import { ref } from 'vue';
 
+function getRandomInt(min: number, max: number): number {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+function generateFileNumber(year: number): string {
+  const randomSegment = getRandomInt(10000, 99999);
+  return `FILE-${year}-OPR-${randomSegment}`;
+}
+
+const thisYear = new Date().getFullYear();
+const fileNumber = ref<string>(generateFileNumber(thisYear));
 </script>
 
 <template>
-  <Hero />
+  <Hero id="hero" :fileNumber="fileNumber"/>
+  <About id="profile" :fileNumber="fileNumber"/>
+  <Skill id="arseanal"/>
+  <Experience id="timeline"/>
+  <SideProjects id="cases"/>
+  <Contact id="contact"/>
 </template>

9410dafaf1bd9152f232910e91bed846dccf00b2
Ray <startup@9043672@gmail.com>
2026-01-24 20:24:08 +0800

    fix:dark mode style

--- src/components/layout/Hero.vue
+++ src/components/layout/Hero.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import { ref,defineProps } from 'vue';
+import { ref,computed,defineProps } from 'vue';
 import Button from '../ui/button/Button.vue';
 
 const lightingSection = ref<HTMLElement|null>(null);
 const lightPosition = ref<{x:string,y:string}>({x:'0px',y:'0px'});
-
+    
 const props = defineProps<{
   fileNumber: string;
 }>();
@@ -18,16 +18,21 @@ const mouseMoveHandler = (e:MouseEvent) => {
 }
 
 const quickOpen = ref<string>('#profile');
-
 document.addEventListener('mousemove', mouseMoveHandler)
 </script>
 
 <template>
   <section ref="lightingSection" class="w-full h-screen bg-[url(@/assets/desk.jpeg)] bg-cover bg-center bg-no-repeat relative">
-    <div class="absolute inset-0 pointer-events-none transition-all duration-75" :style="{background: `radial-gradient(400px at ${lightPosition.x} ${lightPosition.y}, transparent 0%, rgba(0, 0, 0, 0.85) 100%)`}"/>
+    <div 
+      class="absolute inset-0 pointer-events-none transition-all duration-75 
+      bg-[rgba(255,255,255,0.8)]
+      dark:bg-[radial-gradient(400px_at_var(--x)_var(--y),transparent_0%,rgba(0,0,0,0.85)_100%)]"
+      :style="{ '--x': lightPosition.x, '--y': lightPosition.y }"
+    /> 
+    
     <div class="px-[5vw] pt-32 pb-20 max-w-4xl mx-auto flex flex-col relative z-10 text-center">
       <div>
-        <h1 class="text-muted-foreground">{{ props.fileNumber }}</h1>
+        <h1 class="text-muted">{{ props.fileNumber }}</h1>
         <h2 class="mt-5 text-[1rem] sm:text-[3rem] leading-tight">{{$t("hero.fileType")}}</h2>
         <p class="mb-10 text-muted-foreground">{{$t("hero.fileDescription")}}</p>
         <Button class="mb-3">

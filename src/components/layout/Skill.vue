1f2c4c3b69c7864ab7b1aa1fb444a62b1eb0edf0
Ray <startup@9043672@gmail.com>
2026-01-25 21:30:21 +0800

    feat: update color palette and background effects across components, and remove an unused import.

--- src/components/layout/Skill.vue
+++ src/components/layout/Skill.vue
@@ -10,22 +10,26 @@ const { tm } = useI18n();
 
 </script>
 <template>
-  <section class="bg-secondary">
-    <h2 class="text-center text-3xl mb-10">{{ $t('skills.title') }}</h2>
+  <section class="bg-elevated">
+    <h2 class="text-center text-3xl mb-10 text-on-surface">{{ $t('skills.title') }}</h2>
     <div class="grid lg:grid-cols-6 gap-5 mb-10">
-      <Card class="border-base bg-base hover:border-spotlight-yellow relative" :class="Number(index)>2?'col-span-3':'col-span-2'" v-for="(skill, index) in tm('skills.items')" :key="index">
+      <Card class="border-base bg-surface hover:border-spotlight-yellow relative"
+        :class="Number(index) > 2 ? 'col-span-3' : 'col-span-2'" v-for="(skill, index) in tm('skills.items')"
+        :key="index">
         <CardHeader>
           <CardTitle>
-            <h3 class="text-lg text-white">{{ skill.title }}</h3>
+            <h3 class="text-lg text-on-surface">{{ skill.title }}</h3>
           </CardTitle>
           <CardDescription class="absolute -top-3 left-3">
-            <p class="p-1 px-2 text-sm text-on-paper-label bg-paper">class{{ index+1 }}</p>
+            <p class="p-1 px-2 text-sm text-paper-label bg-paper">class{{ index + 1 }}</p>
           </CardDescription>
         </CardHeader>
         <CardContent>
           <ul class="flex flex-wrap gap-3">
-            <li v-for="(item,itemIndex) in skill.items" :key="item" class="w-fit px-3 py-1 border border-base text-muted bg-card hover:text-primary hover:bg-elevated hover:transition-colors hover:cursor-default">
-              <a v-if="skill.urls[itemIndex]" :href="skill.urls[itemIndex]" target="_blank" rel="noopener noreferrer">{{ item }}</a>
+            <li v-for="(item, itemIndex) in skill.items" :key="item"
+              class="w-fit px-3 py-1 border border-base text-muted bg-surface hover:text-primary hover:bg-elevated hover:transition-colors hover:cursor-default">
+              <a v-if="skill.urls[itemIndex]" :href="skill.urls[itemIndex]" target="_blank" rel="noopener noreferrer">{{
+                item }}</a>
               <span v-else>{{ item }}</span>
             </li>
           </ul>

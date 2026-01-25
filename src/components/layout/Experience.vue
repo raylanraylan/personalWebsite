486ae75c4180df0ef1ab761651530381904056fc
Ray <startup@9043672@gmail.com>
2026-01-25 21:35:34 +0800

    refactor: Update UI styling with new color tokens and gradient adjustments across Hero and Experience components, and remove an unused import.

--- src/components/layout/Experience.vue
+++ src/components/layout/Experience.vue
@@ -9,20 +9,22 @@ const { tm } = useI18n();
 </script>
 
 <template>
-  <section class="relative bg-corkboard">
+  <section class="relative bg-cork">
     <h2 class="text-center text-3xl mb-10">{{ $t('experienceTimeline.title') }}</h2>
-    <Card class="block w-[450px] bg-paper rounded-lg mb-10 shadow-xl shadow-amber-800/50 last:mb-0" :class="Number(index)%2 === 0 ? 'mr-auto -rotate-2' : 'ml-auto rotate-2'" v-for="(experience, index) in tm('experienceTimeline.items')" :key="index">
+    <Card class="block w-[450px] bg-paper rounded-lg mb-10 shadow-xl shadow-amber-800/50 last:mb-0"
+      :class="Number(index) % 2 === 0 ? 'mr-auto -rotate-2' : 'ml-auto rotate-2'"
+      v-for="(experience, index) in tm('experienceTimeline.items')" :key="index">
       <CardHeader>
         <CardTitle class="mb-3">
           <span class="block mb-0 text-lg text-inverse">{{ experience.company }}</span>
-          <span class="block mb-1 text-sm text-on-paper-label">{{ experience.title }}</span>
+          <span class="block mb-1 text-sm text-paper-label">{{ experience.title }}</span>
           <span class="block w-fit px-2 py-1 text-xs text-primary rounded bg-surface">{{ experience.timeRange }}</span>
         </CardTitle>
-        <hr class="h-px border-dashed bg-surface"/>
+        <hr class="h-px border-dashed bg-surface" />
       </CardHeader>
       <CardContent>
         <ul class="pl-5 list-[square] space-y-2">
-          <li v-for="(workItem, workIndex) in experience.work" :key="workIndex" class="text-on-paper-label">
+          <li v-for="(workItem, workIndex) in experience.work" :key="workIndex" class="text-paper-label">
             <h3>{{ workItem.title }}</h3>
             <p>{{ workItem.content }}</p>
           </li>

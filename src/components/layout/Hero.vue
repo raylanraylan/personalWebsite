fcdbc4228ef055070522a1b633b4ff773709d24d
Ray <startup@9043672@gmail.com>
2026-01-25 21:35:57 +0800

    style: refine Hero section gradients and color classes, and remove unused import in Contact.

--- src/components/layout/Hero.vue
+++ src/components/layout/Hero.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref,computed,defineProps } from 'vue';
+import { ref,computed,defineProps,inject } from 'vue';
 import Button from '../ui/button/Button.vue';
 
 const lightingSection = ref<HTMLElement|null>(null);
@@ -25,21 +25,21 @@ document.addEventListener('mousemove', mouseMoveHandler)
   <section ref="lightingSection" class="w-full h-screen bg-[url(@/assets/desk.jpeg)] bg-cover bg-center bg-no-repeat relative">
     <div 
       class="absolute inset-0 pointer-events-none transition-all duration-75 
-      bg-[rgba(255,255,255,0.8)]
-      dark:bg-[radial-gradient(400px_at_var(--x)_var(--y),transparent_0%,rgba(0,0,0,0.85)_100%)]"
+      bg-[linear-gradient(180deg,rgba(255,255,255,0.85)_0%,rgba(255,255,255,0.85)_100%)]
+      dark:bg-[radial-gradient(200px_at_var(--x)_var(--y),transparent_0%,rgba(0,0,0,0.85)_100%)]"
       :style="{ '--x': lightPosition.x, '--y': lightPosition.y }"
     /> 
-    
+    <!-- <div class="absolute inset-0 bg-gradient-to-t from-background via-transparent to-background/50"></div> -->
     <div class="px-[5vw] pt-32 pb-20 max-w-4xl mx-auto flex flex-col relative z-10 text-center">
       <div>
-        <h1 class="text-muted">{{ props.fileNumber }}</h1>
-        <h2 class="mt-5 text-[1rem] sm:text-[3rem] leading-tight">{{$t("hero.fileType")}}</h2>
-        <p class="mb-10 text-muted-foreground">{{$t("hero.fileDescription")}}</p>
-        <Button class="mb-3">
+        <h1 class="text-brand-highlight">{{ props.fileNumber }}</h1>
+        <h2 class="mt-5 text-[1rem] text-primary sm:text-[3rem] leading-tight">{{$t("hero.fileType")}}</h2>
+        <p class="mb-10 text-paper-muted">{{$t("hero.fileDescription")}}</p>
+        <Button class="mb-3 bg-overlay text-brand-highlight hover:shadow-glow" >
           <a :href="quickOpen">{{ $t("hero.quickOpen") }}</a>
         </Button>
       </div>
-      <p class="absolute bottom-0 left-1/2 transform -translate-x-1/2">{{ $t("hero.useFile") }}</p>
+      <p class="absolute bottom-0 left-1/2 transform -translate-x-1/2 text-paper-muted text-sm">{{ $t("hero.useFile") }}</p>
     </div> 
   </section> 
 </template>

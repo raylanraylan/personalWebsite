adc25a40487d450f0f0f2076cb65c6e43a2d2f20
Ray <startup@9043672@gmail.com>
2026-01-25 22:00:42 +0800

    style: adjust skill card background and title text colors.

--- src/components/layout/Skill.vue
+++ src/components/layout/Skill.vue
@@ -13,12 +13,12 @@ const { tm } = useI18n();
   <section class="bg-elevated">
     <h2 class="text-center text-3xl mb-10 text-on-surface">{{ $t('skills.title') }}</h2>
     <div class="grid lg:grid-cols-6 gap-5 mb-10">
-      <Card class="border-base bg-surface hover:border-spotlight-yellow relative"
+      <Card class="border-base bg-primary hover:border-spotlight-yellow relative"
         :class="Number(index) > 2 ? 'col-span-3' : 'col-span-2'" v-for="(skill, index) in tm('skills.items')"
         :key="index">
         <CardHeader>
           <CardTitle>
-            <h3 class="text-lg text-on-surface">{{ skill.title }}</h3>
+            <h3 class="text-lg text-surface">{{ skill.title }}</h3>
           </CardTitle>
           <CardDescription class="absolute -top-3 left-3">
             <p class="p-1 px-2 text-sm text-paper-label bg-paper">class{{ index + 1 }}</p>

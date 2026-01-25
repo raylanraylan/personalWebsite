2b99f8396cf5f36844a63cf71d4c4f8cdae89e22
Ray <startup@9043672@gmail.com>
2026-01-25 22:00:27 +0800

    refactor: Update UI styles for Skill and SideProject cards, adjusting background, text colors, and hover effects.

--- src/components/layout/SideProjects.vue
+++ src/components/layout/SideProjects.vue
@@ -11,15 +11,16 @@ const { tm } = useI18n();
     <h2 class="text-center text-3xl mb-10">{{ $t('project.title') }}</h2>
     <div class="grid gap-10 lg:grid-cols-2">
       <Card v-for="project in tm('project.items')" :key="project.title"
-        class="max-w-4xl bg-manila rounded-lg shadow-noir transition-all duration-500 opacity-100 transform last:mb-0 hover:-translate-y-2 hover:shadow-glow">
+        class="max-w-4xl bg-manila rounded-lg shadow-noir transition-all duration-500 opacity-100 transform last:mb-0 hover:-translate-y-2 hover:border-spotlight-yellow hover:border-2">
         <CardHeader>
           <CardTitle class="text-lg text-ink-black group-hover:text-alert-red">{{ project.title }}</CardTitle>
           <CardDescription>
             <p class="text-paper-label">{{ project.description }}</p>
             <br />
             <Button>
-              <a class="text-inverse hover:underline" :href="project.url" target="_blank" rel="noopener noreferrer">{{
-                $t('project.view') }}</a>
+              <a class="text-paper-muted hover:underline" :href="project.url" target="_blank"
+                rel="noopener noreferrer">{{
+                  $t('project.view') }}</a>
             </Button>
           </CardDescription>
         </CardHeader>

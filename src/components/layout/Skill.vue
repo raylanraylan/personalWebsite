446cd5a4ff05a51280a8ea870219acb3e0d258ff
Ray <startup@9043672@gmail.com>
2026-01-27 22:13:42 +0800

    feat: Implement responsive styling for the skills section, adjusting text sizes, margins, and padding across different screen sizes.

--- src/components/layout/Skill.vue
+++ src/components/layout/Skill.vue
@@ -11,23 +11,23 @@ const { tm } = useI18n();
 </script>
 <template>
   <section class="bg-elevated">
-    <h2 class="text-center text-3xl mb-10 text-on-surface">{{ $t('skills.title') }}</h2>
-    <div class="grid grid-cols-1 gap-y-5 sm:gap-5 sm:grid-cols-2 lg:grid-cols-6 lg:gap-5 mb-10">
+    <h2 class="text-center text-xl sm:text-3xl mb-3 sm:mb-10 text-on-surface">{{ $t('skills.title') }}</h2>
+    <div class="grid grid-cols-1 gap-y-5 mb-3 sm:gap-5 sm:grid-cols-2 sm:mb-10lg:grid-cols-6 lg:gap-5">
       <Card class="border-base bg-primary hover:border-spotlight-yellow relative"
         :class="Number(index) > 2 ? 'lg:col-span-3' : 'lg:col-span-2'" v-for="(skill, index) in tm('skills.items')"
         :key="index">
-        <CardHeader>
+        <CardHeader class="p-4 sm:p-6">
           <CardTitle>
-            <h3 class="text-lg text-surface">{{ skill.title }}</h3>
+            <h3 class="text-sm sm:text-lg text-surface">{{ skill.title }}</h3>
           </CardTitle>
           <CardDescription class="absolute -top-3 left-3">
-            <p class="p-1 px-2 text-sm text-paper-label bg-paper">class{{ index + 1 }}</p>
+            <p class="p-1 px-2 text-xs sm:text-sm text-paper-label bg-paper">class{{ index + 1 }}</p>
           </CardDescription>
         </CardHeader>
-        <CardContent>
+        <CardContent class="p-4 sm:p-6">
           <ul class="flex flex-wrap gap-3">
             <li v-for="(item, itemIndex) in skill.items" :key="item"
-              class="w-fit px-3 py-1 border border-base text-muted bg-surface hover:text-primary hover:bg-elevated hover:transition-colors hover:cursor-default">
+              class="w-fit px-3 py-1 border border-base text-xs sm:text-base text-muted bg-surface hover:text-primary hover:bg-elevated hover:transition-colors hover:cursor-default">
               <a v-if="skill.urls[itemIndex]" :href="skill.urls[itemIndex]" target="_blank" rel="noopener noreferrer">{{
                 item }}</a>
               <span v-else>{{ item }}</span>
@@ -36,6 +36,6 @@ const { tm } = useI18n();
         </CardContent>
       </Card>
     </div>
-    <p class="text-center text-sm text-gray-500">{{ $t('skills.description') }}</p>
+    <p class="text-center text-xs sm:text-sm text-gray-500">{{ $t('skills.description') }}</p>
   </section>
 </template>

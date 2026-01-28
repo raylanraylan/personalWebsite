27e26731fa022ee4376c983ea3bee8f7ffd46121
Ray <startup@9043672@gmail.com>
2026-01-28 21:06:48 +0800

    refactor: Replace desk.jpeg with desk.jpg and update its import in the Hero component.

--- src/components/layout/Hero.vue
+++ src/components/layout/Hero.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { ref, computed, defineProps, inject } from 'vue';
 import Button from '../ui/button/Button.vue';
-import deskImg from '@/assets/desk.jpeg';
+import deskImg from '@/assets/desk.jpg';
 const lightingSection = ref<HTMLElement | null>(null);
 const lightPosition = ref<{ x: string, y: string }>({ x: '0px', y: '0px' });
 

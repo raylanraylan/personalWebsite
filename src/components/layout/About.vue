e424f0538aa16406cc6adc6f19ad8d837858eb40
Ray <startup@9043672@gmail.com>
2026-02-06 00:19:59 +0800

    feat: Implement a new desktop header layout and introduce image utility composables for URL resolution and aspect ratio calculation.

--- src/components/layout/About.vue
+++ src/components/layout/About.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-import { defineProps, ref, onMounted } from 'vue';
-import paperImg from '@/assets/images/paper.webp';
+import { defineProps, ref, onMounted, type Ref } from 'vue';
 
 import Card from '../ui/card/Card.vue';
 import CardHeader from '../ui/card/CardHeader.vue';
@@ -13,14 +12,34 @@ const props = defineProps<{
   fileNumber: string;
 }>();
 
-const aspectRatio = ref('auto');
+function imgUrl(url: string) {
+  return new URL(url, import.meta.url).href
+}
 
-onMounted(() => {
-  const img = new Image();
-  img.src = paperImg;
-  img.onload = () => {
-    aspectRatio.value = `${img.naturalWidth} / ${img.naturalHeight}`;
-  }
+const paperImgUrl = imgUrl('/src/assets/images/paper.webp');
+const aspectRatio = ref('auto');
+const penImgUrl = imgUrl('/src/assets/images/pen.webp');
+const cameraImgUrl = imgUrl('/src/assets/images/camera.webp');
+
+// 這樣寫 await 才有意義
+function imgSize(url: string, variable?: Ref<string>): Promise<void> {
+  return new Promise((resolve) => {
+    const img = new Image();
+    img.src = url;
+    img.onload = () => {
+      if (variable) {
+        variable.value = `${img.naturalWidth} / ${img.naturalHeight}`;
+      }
+      resolve(); // 載入完成，結束 Promise
+    };
+    img.onerror = () => resolve(); // 失敗也要結束，避免 await 卡死
+  });
+}
+
+onMounted(async () => {
+  await imgSize(paperImgUrl, aspectRatio);
+  await imgSize(penImgUrl);
+  await imgSize(cameraImgUrl);
 });
 
 const updatedDate = '2026-01-15';
@@ -35,21 +54,20 @@ const aboutItems = [
 
 </script>
 <template>
-  <section class="bg-[linear-gradient(180deg, hsl(0 0% 1% / 1), transparent)]">
+  <section class="bg-[linear-gradient(180deg, hsl(0 0% 1% / 1), transparent)] relative">
     <h2 class="text-center text-xl mb-3 sm:text-3xl sm:mb-10 lg:hidden">{{ $t('profile_section') }}</h2>
-    <Card :style="{ aspectRatio: aspectRatio }"
-      class="bg-paper lg:bg-transparent lg:bg-[url('@/assets/images/paper.webp')] bg-size-[100%_100%] h-auto bg-top bg-no-repeat sm:p-12 lg:px-30 py-20 relative lg:border-0 lg:shadow-[px_2px_5px_2px_rgba(255,255,255,1)]">
+    <Card :style="{ aspectRatio: aspectRatio, backgroundImage: `url(${paperImgUrl})` }"
+      class="bg-paper lg:bg-transparent bg-size-[100%_100%] h-auto bg-top sm:p-12 lg:px-30 py-20 relative lg:border-0 lg:bg-no-repeat">
       <CardHeader class="p-0">
         <h3
-          class="sm:text-5xl status-danger p-4 border-2 border-danger uppercase absolute right-[5%] top-[10%] rotate-25 lg:opacity-80 text-shadow-sm text-shadow-red-500 mix-blend-color-burn filter-blur-sm">
-          {{
-            $t('about.stamp') }}</h3>
+          class="sm:text-5xl status-danger p-4 border-2 border-danger uppercase absolute right-[5%] top-[10%] rotate-25 text-shadow-sm text-shadow-red-500 mix-blend-color-burn filter-blur-sm">
+          {{ $t('about.stamp') }}</h3>
         <CardTitle>
           <h2
             class="text-center text-xl mb-3 sm:text-3xl sm:mb-10 lg:text-ink-black lg:opacity-80 lg:text-shadow-xs lg:text-shadow-black hidden lg:block">
             {{ $t('profile_section') }}</h2>
         </CardTitle>
-        <CardDescription class="lg:mix-blend-darken lg:opacity-80 lg:text-shadow-xs lg:text-shadow-black">
+        <CardDescription class="lg:mix-blend-multiply lg:opacity-80 lg:text-shadow-xs lg:text-shadow-black">
           <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 lg:gap-0 lg:border-t-2 lg:border-l-2 lg:border-gray-800">
             <div v-for="item in aboutItems" :key="item" class="lg:p-3 lg:border-b-2 lg:border-r-2 lg:border-gray-800"
               :class="item === 'name' ? 'sm:col-span-2' : ''">
@@ -73,5 +91,7 @@ const aboutItems = [
         <span>{{ $t('about.footer.updated') }}{{ $t('about.colon') }}{{ updatedDate }}</span>
       </CardFooter>
     </Card>
+    <img :src="penImgUrl" class="absolute right-[20%] bottom-[10%] rotate-45" />
+    <img :src="cameraImgUrl" class="absolute left-0 bottom-0" />
   </section>
 </template>

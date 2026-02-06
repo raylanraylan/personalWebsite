22579c3c5c619717d6ad4606af7cab1a570cf7b0
Ray <startup@9043672@gmail.com>
2026-02-06 19:18:49 +0800

    feat: Redesign desktop header with vertical navigation and refresh 404 page styling.

--- src/views/PathNotFound.vue
+++ src/views/PathNotFound.vue
@@ -2,21 +2,23 @@
 import { onMounted } from 'vue';
 import { RouterLink } from 'vue-router';
 
-onMounted(()=>{
-  if(localStorage.getItem('darkMode') === 'true'){
+onMounted(() => {
+  if (localStorage.getItem('darkMode') === 'true') {
     document.documentElement.classList.add('dark');
   }
 })
 </script>
 <template>
-  <main class="grid min-h-full place-items-center px-6 lg:px-8">
-    <div class="text-center text-gray-900 dark:text-white">
-      <p class="text-base font-semibold">404</p>
-      <h1 class="mt-4 text-3xl font-bold tracking-tight sm:text-5xl">{{ $t("notFound.title") }}</h1>
-      <p class="mt-6 text-base leading-7">{{ $t("notFound.content") }}</p>
+  <div class="h-screen flex items-center justify-center">
+    <div class="text-center">
+      <p class="text-base">404</p>
+      <h1 class="mt-4 text-3xl font-bold sm:text-5xl">{{ $t("notFound.title") }}</h1>
+      <p class="mt-6 text-base">{{ $t("notFound.content") }}</p>
       <div class="mt-10 flex items-center justify-center gap-x-6">
-        <RouterLink to="/" class="rounded-md bg-gray-900 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-amber-400 focus-visible:outline-solid focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:bg-white dark:text-gray-900">{{ $t("notFound.goBackHome") }}</RouterLink>
+        <RouterLink to="/"
+          class="rounded-md px-3.5 py-2.5 text-sm font-semibold bg-muted text-brand-highlight hover:shadow-glow">
+          {{ $t("notFound.goBackHome") }}</RouterLink>
       </div>
     </div>
-  </main>
+  </div>
 </template>
